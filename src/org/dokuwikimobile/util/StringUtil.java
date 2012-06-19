c1bdb70728e9e7ade6d6df424e3986e991490734
Tim Roes <mail@timroes.de>
2012-06-19 15:03:44 +0200

    Documented StringUtil

--- src/org/dokuwikimobile/util/StringUtil.java
+++ src/org/dokuwikimobile/util/StringUtil.java
@@ -4,12 +4,20 @@ import java.util.Iterator;
 
 /**
  *
- * @author Tim Roes
+ * @author Tim Roes <mail@timroes.de>
  */
 public class StringUtil {
 	
 	private final static String[] SHORT_UNITS = new String[]{ "Bytes","kB","MB","GB","TB" };
 	
+	/**
+	 * Prints out a file size in a human readable format. This will return the
+	 * file size with a trailing size unit and rounded to the given decimal places.
+	 * 
+	 * @param filesize The file size given in bytes.
+	 * @param decimal The decimal places that should be shown.
+	 * @return The {@link String} representing the file size in a human readable format.
+	 */
 	public static String humanReadable(int filesize, int decimal) {
 		
 		if(filesize == 0) {
@@ -23,6 +31,14 @@ public class StringUtil {
 
 	}
 	
+	/**
+	 * Joins a {@link String}-Array with a given sequence of characters into one
+	 * string.
+	 * 
+	 * @param stringArray The {@link String}-Array which elements should be joined.
+	 * @param joinChar The {@link String} to used for joining the elements.
+	 * @return The concatenated {@link String}.
+	 */
 	public static String join(Iterable<String> stringArray, String joinChar) {
 
 		StringBuilder builder = new StringBuilder();
@@ -41,6 +57,12 @@ public class StringUtil {
 		
 	}
 
+	/**
+	 * Checks whether the given {@link String} is null or empty.
+	 * 
+	 * @param str The {@link String} that is checked.
+	 * @return Whether the {@link String} is null or empty.
+	 */
 	public static boolean isNullOrEmpty(String str) {
 		return str == null || str.isEmpty();
 	}
